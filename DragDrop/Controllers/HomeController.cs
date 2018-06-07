c0437f3062dcb015b093cd851d44c57a2a786829
Saeed Gorbani <sgorbani@wilshire.com>
2018-06-07 10:40:28 -0700

    Add project files.

---
+++ DragDrop/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+namespace DragAndDrop.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private const string TempPath = @"D:\neethu\DragAndDrop\DragAndDrop\App_Data";
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<System.Web.HttpPostedFileBase> files)
+        {
+            foreach (HttpPostedFileBase file in files)
+            {
+                string filePath = Path.Combine(TempPath, file.FileName);
+                System.IO.File.WriteAllBytes(filePath, ReadData(file.InputStream));
+            }
+
+            return Json("All files have been successfully stored.");
+        }
+
+        private byte[] ReadData(Stream stream)
+        {
+            byte[] buffer = new byte[16 * 1024];
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
+
